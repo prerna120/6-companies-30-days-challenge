96a1664588c4ab773829206c1e3ce5acb2289f21
prerna120 <50449860+prerna120@users.noreply.github.com>
2022-01-02 17:21:44 +0530

    Create 7.cpp

---
+++ Goldman Sachs/7.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+  public:
+    int findPosition(int N , int M , int K) {
+        // code here
+        M--;
+        int ans=(K+M%N)%N;
+        if(ans==0)
+            return N;
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,M,K;
+        
+        cin>>N>>M>>K;
+
+        Solution ob;
+        cout << ob.findPosition(N,M,K) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
