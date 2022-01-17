064e3580e9660a8ea4cd1cfd5205d7fad3792abf
prerna120 <50449860+prerna120@users.noreply.github.com>
2022-01-17 23:55:27 +0530

    Create 4.cpp

---
+++ Adobe/4.cpp
@@ -0,0 +1,54 @@
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+
+
+    int equalPartition(int N, int arr[])
+    {
+        // code here
+        int sum=0;
+        for(int i=0;i<N;i++){
+            sum+=arr[i];
+        }
+        if(sum%2!=0)
+            return false;
+        sum=sum/2;
+        vector<bool>v(sum+1,false);
+        v[0]=true;
+        for(int i=0;i<N;i++){
+            for(int j=sum;j>=1;j--){
+                if(arr[i]<=j)
+                    v[j]=v[j] || v[j-arr[i]];
+            }
+        }
+        return v[sum];
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        if(ob.equalPartition(N, arr))
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}  // } Driver Code Ends
