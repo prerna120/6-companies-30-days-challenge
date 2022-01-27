348f536b6bc1fdc443dfe2ae804d062651c03f09
prerna120 <50449860+prerna120@users.noreply.github.com>
2022-01-27 20:18:15 +0530

    Create 8.cpp

---
+++ Walmart/8.cpp
@@ -0,0 +1,34 @@
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int height(int N){
+        // code here
+        for(int i=1;i<10000;i++){
+            if((i*(i+1))/2>N)
+                return i-1;
+        }
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.height(N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
